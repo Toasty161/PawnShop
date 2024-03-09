7ef5998a03adf9efd0673ee55fbc6879850a3ec7
Toasty <mrtoasty161@gmail.com>
2024-03-09 20:41:35 +0200

    Added the DbSets in ApplicationDbContext and configured database relationships in OnModelCreating method

--- PawnShop.Infrastructure/Data/ApplicationDbContext.cs
+++ PawnShop.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PawnShop.Infrastructure.Data.Models;
 
 namespace PawnShop.Data
 {
@@ -8,6 +9,51 @@ namespace PawnShop.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Possession> Possessions { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<PossessionBuyer> PossessionBuyers { get; set; }
+
+        public DbSet<ProductBuyer> ProductBuyers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ProductBuyer>()
+                .HasKey(c => new { c.BuyerId, c.ProductId });
+
+            builder.Entity<ProductBuyer>()
+                .HasOne(c => c.Product)
+                .WithMany()
+                .HasForeignKey(c => c.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ProductBuyer>()
+                .HasOne(c => c.Buyer)
+                .WithMany()
+                .HasForeignKey(c => c.BuyerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<PossessionBuyer>()
+                .HasKey(c => new { c.BuyerId, c.PossessionId });
+
+            builder.Entity<PossessionBuyer>()
+                .HasOne(c => c.Possession)
+                .WithMany()
+                .HasForeignKey(c => c.PossessionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<PossessionBuyer>()
+                .HasOne(c => c.Buyer)
+                .WithMany()
+                .HasForeignKey(c => c.BuyerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
         }
     }
 }
