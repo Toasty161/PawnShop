f564c4aa5b2f030944739b95b045730cfb372152
Toasty <mrtoasty161@gmail.com>
2024-03-09 21:57:12 +0200

    Added Possession and ProductViewModels for the services

---
+++ PawnShop.Core/Models/PossessionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PawnShop.Infrastructure.Data.Constants.DataConstants;
+
+namespace PawnShop.Core.Models
+{
+    public class PossessionViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(PossessionNameMaxLength, MinimumLength = PossessionNameMinLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(PossessionMinArea, PossessionMaxArea)]
+        public double Area { get; set; }
+
+        [Required]
+        [StringLength(PossessionLocationMaxLength, MinimumLength = PossessionLocationMinLength)]
+        public string Location { get; set; } = string.Empty;
+
+        [Required]
+        public string OwnerId { get; set; } = string.Empty;
+    }
+}
---
+++ PawnShop.Core/Models/ProductViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PawnShop.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PawnShop.Infrastructure.Data.Constants.DataConstants;
+
+namespace PawnShop.Core.Models
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(ProductNameMaxLength, MinimumLength = ProductNameMinLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ProductDescriptionMaxLength, MinimumLength = ProductDescriptionMinLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Range(ProductMinWeight, ProductMaxWeight)]
+        public double Weight { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; } = string.Empty;
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
