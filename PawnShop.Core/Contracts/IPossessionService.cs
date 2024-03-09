f25a1f49cebee134dd552d044265b47ab3483d5f
Toasty <mrtoasty161@gmail.com>
2024-03-09 21:49:08 +0200

    Added service interfaces for entity Product and Possession

---
+++ PawnShop.Core/Contracts/IPossessionService.cs
@@ -0,0 +1,20 @@
+﻿using PawnShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnShop.Core.Contracts
+{
+    public interface IPossessionService
+    {
+        Task AddAsync(PossessionViewModel possessionViewModel);
+
+        Task DeleteAsync(int id);
+
+        Task EditAsync(PossessionViewModel possessionViewModel);
+
+        Task<PossessionViewModel> GetByIdAsync(int id);
+    }
+}
---
+++ PawnShop.Core/Contracts/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using PawnShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnShop.Core.Contracts
+{
+    public interface IProductService
+    {
+        Task AddAsync(ProductViewModel productViewModel);
+
+        Task DeleteAsync(int id);
+
+        Task EditAsync(ProductViewModel productViewModel);
+
+        Task<ProductViewModel> GetByIdAsync(int id);
+    }
+}
