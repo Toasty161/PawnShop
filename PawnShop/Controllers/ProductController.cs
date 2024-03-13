f48c98a7edebef0f2de63b3ab7fac815f439ca4e
Toasty <mrtoasty161@gmail.com>
2024-03-13 23:42:16 +0200

    Added ProductController for interaction with the products and added All.cshtml to view the current available products

---
+++ PawnShop/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PawnShop.Core.Models;
+using PawnShop.Core.Services;
+using PawnShop.Data;
+
+namespace PawnShop.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ProductService _productService;
+
+        public ProductController(ApplicationDbContext context, ProductService productService)
+        {
+            _context = context;
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var entities = await _context.Products.ToListAsync();
+
+            var model = entities.Select(p => new ProductViewModel()
+            {
+                CategoryId = p.CategoryId,
+                Name = p.Name,
+                Description = p.Description,
+                Id = p.Id,
+                OwnerId = p.OwnerId,
+                Weight = p.Weight,
+                Category = _context.Categories.Find(p.CategoryId)!.Name
+            }).ToList();
+
+            return View(model);
+        }
+    }
+}
