f5e5651194a8e24b92565f86b0707bb4423ad356
Toasty <mrtoasty161@gmail.com>
2024-03-31 19:06:40 +0300

    Added All page for the possessions

---
+++ PawnShop/Controllers/PossessionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PawnShop.Core.Models;
+using PawnShop.Core.Services;
+using PawnShop.Data;
+
+namespace PawnShop.Controllers
+{
+	[Authorize]
+	public class PossessionController : Controller
+	{
+		private readonly PossessionService _possessionService;
+		private readonly ApplicationDbContext _context;
+
+		public PossessionController(PossessionService possessionService, ApplicationDbContext context)
+		{
+			_possessionService = possessionService;
+			_context = context;
+		}
+
+		public async Task<IActionResult> All()
+		{
+			var model = await _context.Possessions.Select(p => new PossessionViewModel()
+			{
+				Area = p.Area,
+				Id = p.Id,
+				Location = p.Location,
+				Name = p.Name,
+				OwnerId = p.OwnerId
+			}).ToListAsync();
+
+			return View(model);
+		}
+	}
+}
