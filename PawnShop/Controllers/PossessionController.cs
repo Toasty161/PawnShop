a368483068fc6c4692d5c5d75ce1e848cb4e29d1
Toasty <mrtoasty161@gmail.com>
2024-03-31 19:58:02 +0300

    Added Edit functionality for the possession

--- PawnShop/Controllers/PossessionController.cs
+++ PawnShop/Controllers/PossessionController.cs
@@ -55,6 +55,37 @@ namespace PawnShop.Controllers
 			return RedirectToAction(nameof(All));
 		}
 
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			if (await _context.Possessions.FindAsync(id) == null)
+			{
+				return RedirectToAction(nameof(All));
+			}
+
+			var model = await _possessionService.GetByIdAsync(id);
+
+			TempData["EditPossessionId"] = id;
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(PossessionViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return RedirectToAction(nameof(All));
+			}
+
+			model.OwnerId = GetUserId();
+			model.Id = (int)TempData["EditPossessionId"];
+
+			await _possessionService.EditAsync(model);
+
+            return RedirectToAction(nameof(All));
+        }
+
 		private string GetUserId()
 		{
 			return User.FindFirst(ClaimTypes.NameIdentifier).Value;
