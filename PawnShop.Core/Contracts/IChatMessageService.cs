9498dfe3bdbc1f6977f358c7ff394eaa27b805c2
Toasty <mrtoasty161@gmail.com>
2024-04-04 23:44:09 +0300

    Added contracts for the ChatMessage and ContactMessage services

---
+++ PawnShop.Core/Contracts/IChatMessageService.cs
@@ -0,0 +1,20 @@
+﻿using PawnShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnShop.Core.Contracts
+{
+    public interface IChatMessageService
+    {
+        Task AddAsync(ChatMessageViewModel model);
+
+        Task DeleteAsync(int id);
+
+        Task EditAsync(ChatMessageViewModel model);
+
+        Task<ChatMessageViewModel> GetByIdAsync(int id);
+    }
+}
---
+++ PawnShop.Core/Contracts/IContactMessageService.cs
@@ -0,0 +1,20 @@
+﻿using PawnShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnShop.Core.Contracts
+{
+    public interface IContactMessageService
+    {
+        Task AddAsync(ContactMessageViewModel model);
+
+        Task EditAsync(ContactMessageViewModel model);
+
+        Task DeleteAsync(int id);
+
+        Task<ContactMessageViewModel> GetByIdAsync(int id);
+    }
+}
